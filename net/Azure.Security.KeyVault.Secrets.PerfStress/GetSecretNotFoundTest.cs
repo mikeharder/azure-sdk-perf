ef32acaa2d8d6683869e05fdda8718efdc5ed934
Mike Harder <mharder@microsoft.com>
2019-11-21 16:22:33 -0800

    Implement SecretNotFound test

--- net/Azure.Security.KeyVault.Secrets.PerfStress/GetSecretNotFoundTest.cs
+++ net/Azure.Security.KeyVault.Secrets.PerfStress/GetSecretNotFoundTest.cs
@@ -1,24 +1,41 @@
-﻿using Azure.Test.PerfStress;
+﻿using Azure.Security.KeyVault.Secrets.PerfStress.Core;
+using Azure.Test.PerfStress;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Azure.Security.KeyVault.Secrets.PerfStress
 {
-    public class GetSecretNotFoundTest : PerfStressTest<PerfStressOptions>
+    public class GetSecretNotFoundTest : SecretTest<PerfStressOptions>
     {
+        private const string _secretName = "GetSecretNotFoundName";
+
         public GetSecretNotFoundTest(PerfStressOptions options) : base(options)
         {
         }
 
         public override void Run(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var _ = SecretClient.GetSecret(_secretName).Value.Value;
+                throw new InvalidOperationException("Expected RequestFailedException");
+            }
+            catch (RequestFailedException ex) when (ex.Status == 404)
+            {
+            }
         }
 
-        public override Task RunAsync(CancellationToken cancellationToken)
+        public override async Task RunAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var _ = (await SecretClient.GetSecretAsync(_secretName)).Value.Value;
+                throw new InvalidOperationException("Expected RequestFailedException");
+            }
+            catch (RequestFailedException ex) when (ex.Status == 404)
+            {
+            }
         }
     }
 }

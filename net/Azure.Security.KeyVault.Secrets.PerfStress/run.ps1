522bfa7787247a8a07bb9ae59a563ef63c48df55
Mike Harder <mharder@microsoft.com>
2019-11-21 16:10:48 -0800

    Add env vars

--- net/Azure.Security.KeyVault.Secrets.PerfStress/run.ps1
+++ net/Azure.Security.KeyVault.Secrets.PerfStress/run.ps1
@@ -1,3 +1,3 @@
 #!/usr/bin/env pwsh
 
-docker run -it --rm --network host -e STORAGE_CONNECTION_STRING azure-security-keyvault-secrets-perfstress-net @args
+docker run -it --rm --network host -e KEYVAULT_URI -e AZURE_CLIENT_ID -e AZURE_CLIENT_SECRET -e AZURE_TENANT_ID azure-security-keyvault-secrets-perfstress-net @args

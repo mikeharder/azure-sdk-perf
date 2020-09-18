6c9e86cfa83fd484926835c3ed375ff7b272fe99
Pavel Krymets <pavel@krymets.com>
2020-09-18 15:42:40 -0700

    It ain't pretty but it compiles

--- net/System.PerfStress/DelayTest.cs
+++ net/System.PerfStress/DelayTest.cs
@@ -21,13 +21,13 @@ namespace Azure.Test.PerfStress
         public override void Run(CancellationToken cancellationToken)
         {
             Thread.Sleep(_delay);
-            _delay *= Options.IterationGrowthFactor;
+            _delay = new TimeSpan((long)(_delay.Ticks * Options.IterationGrowthFactor));
         }
 
         public override async Task RunAsync(CancellationToken cancellationToken)
         {
             await Task.Delay(_delay, cancellationToken);
-            _delay *= Options.IterationGrowthFactor;
+            _delay = new TimeSpan((long)(_delay.Ticks * Options.IterationGrowthFactor));
         }
 
         public class DelayOptions : PerfStressOptions {
@@ -37,7 +37,7 @@ namespace Azure.Test.PerfStress
             // Used for verifying the perf framework correctly computes average throughput across parallel tests of different speed.
             // Each instance of this test completes operations at a different rate, to allow for testing scenarios where
             // some instances are still waiting when time expires.  The first instance completes in 1 second per operation,
-            // the second instance in 2 seconds, the third instance in 4 seconds, and so on. 
+            // the second instance in 2 seconds, the third instance in 4 seconds, and so on.
             [Option("instanceGrowthFactor", Default = 1, HelpText = "Instance growth factor.  The delay of instance N will be (InitialDelayMS * (InstanceGrowthFactor ^ InstanceCount)).")]
             public double InstanceGrowthFactor { get; set; }
 

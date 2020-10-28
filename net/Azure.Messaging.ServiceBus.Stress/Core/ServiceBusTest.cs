97cdac2f3f081655a1e2e0c742cca8455769f2c6
Mike Harder <mharder@microsoft.com>
2020-10-27 17:37:32 -0700

    Sample stress test for ServiceBus (#16)

---
+++ net/Azure.Messaging.ServiceBus.Stress/Core/ServiceBusTest.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Test.Stress;
+using System;
+using System.Threading.Tasks;
+
+namespace Azure.Messaging.ServiceBus.Stress.Core
+{
+    public abstract class ServiceBusTest<TOptions, TMetrics> : StressTest<TOptions, TMetrics> where TOptions: StressOptions where TMetrics : StressMetrics
+    {
+        protected string ServiceBusConnectionString { get; private set; }
+        protected string QueueName { get; private set; }
+
+        protected ServiceBusClient ServiceBusClient { get; private set; }
+        
+        protected ServiceBusTest(TOptions options, TMetrics metrics) : base(options, metrics)
+        {
+            ServiceBusConnectionString = Environment.GetEnvironmentVariable("SERVICE_BUS_CONNECTION_STRING");
+            QueueName = Environment.GetEnvironmentVariable("QUEUE_NAME");
+
+            ServiceBusClient = new ServiceBusClient(ServiceBusConnectionString);
+        }
+
+        public override async ValueTask DisposeAsyncCore()
+        {
+            await ServiceBusClient.DisposeAsync();
+            await base.DisposeAsyncCore();
+        }
+
+    }
+}
